8ddd45dbb02a8584e8199b4a70c850e70289d16e
1solovedeng <244661478@qq.com>
2025-05-13 14:20:48 +0800

    Site updated: 2025-05-13 14:20:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/20/实用工具-Dev-C++/","2025/05/07/力扣-1-两数之和（Two-Sum）/"];function toRandomPost(){
+var posts=["2025/05/07/力扣-1-两数之和（Two-Sum）/","2025/04/20/实用工具-Dev-C++/","2025/05/13/项目推荐-SDL太空战机射击游戏-SDLShooter/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
