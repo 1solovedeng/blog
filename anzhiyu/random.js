0a44d56db74115250e30b41ccdafda806fe96cc9
1solovedeng <244661478@qq.com>
2025-08-30 20:45:59 +0800

    Site updated: 2025-08-30 20:45:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/力扣-1-两数之和（Two-Sum）/","posts/力扣-2-两数相加（Add-Two-Numbers）/","posts/力扣-3-无重复字符的最长子串（Longest-Substring-Without-Repeating-Characters）/","posts/实用工具-Dev-C++/","posts/综合声明-2025-05-20/","posts/项目推荐-SDL太空战机射击游戏-SDLShooter/"];function toRandomPost(){
+var posts=["posts/力扣-2-两数相加（Add-Two-Numbers）/","posts/力扣-3-无重复字符的最长子串（Longest-Substring-Without-Repeating-Characters）/","posts/力扣-1-两数之和（Two-Sum）/","posts/综合声明-2025-05-20/","posts/实用工具-Dev-C++/","posts/项目推荐-SDL太空战机射击游戏-SDLShooter/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
